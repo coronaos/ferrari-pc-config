170a683078f0aec2c311e003d5a0e9479a54ccd6
Maties Lopez <113587392+coronaos@users.noreply.github.com>
2024-05-20 18:25:24 +0200

    Scripts

---
+++ stoprec.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+//systemctl list-unit-files –-type service --all
+proc=("alsa-utils.service" "anacron.service" "apparmor.service" "avahi-daemon.service" "bluetooth.service" "create_ap.service" "cron.service" "cryptdisks-early.service" 
+"cryptdisks.service" "cups-browsed.service" "cups.service");
+
+for i in {0..10}
+do
+sudo systemctl stop ${proc[i]};
+sudo systemctl disable ${proc[i]};
+echo "ejecucin numero: ${proc[i]}";
+done
